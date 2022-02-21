e8af3033537901a39ea0ce6e5247df6c112d31bd
Sethhhhhhh <yohannvvt@gmail.com>
2022-02-21 01:20:00 +0100

    add features, possibility and blog containers.

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './article.css'
 
-const Article = () => {
-  return (
-	<div>Article</div>
-  )
+const Article = ({ imageURL, date, title }) => {
+	return (
+		<div className="gpt3__article">
+			<div className="gpt3__article-image">
+				<img src={imageURL} alt="blog" />
+			</div>
+			<div className="gpt3__article-content">
+				<div>
+					<p>{date}</p>
+					<h3>{title}</h3>
+				</div>
+				<p>Read Full Article</p>
+			</div>
+		</div>
+	)
 }
 
 export default Article
